0a8eaf9f438063d7c6bffccfa9c5e67ab74259b4
hshockney <shockney@atlanticbb.net>
2019-07-26 11:26:53 -0400

    Another explicit subject example

--- spec/explicit_subject_spec.rb
+++ spec/explicit_subject_spec.rb
@@ -14,3 +14,17 @@ RSpec.describe Hash do
         end
     end
 end
+
+RSpec.describe Array do
+    subject(:sally) { [3, 5] }
+    
+    it 'caches the object within an example' do
+      expect(subject.length).to eq(2)
+      subject.pop
+      expect(subject.length).to eq(1)
+    end
+  
+    it 'creates a new object for a new example' do
+      expect(sally).to eq([3, 5])
+    end
+  end
